c0ad9bbe0a6526c01354a36b74de7e99e5fbdd2c
Stefano <sleto1999@icloud.com>
2024-04-06 12:02:46 +0200

    Fixes

--- encryptKey.js
+++ encryptKey.js
@@ -4,16 +4,14 @@ require("dotenv").config()
 
 async function main() {
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
-    
-    const encryptedJsonKey = await wallet.encrypt(
+
+    /* const encryptedJsonKey = await wallet.encrypt(
         process.env.PRIVATE_KEY_PASSWORD,
         process.env.PRIVATE_KEY
-    )
-    // In later version (^6.2.3 as of this commit) of etherjs, PRIVATE_KEY is inferred from wallet, so there is no need to 
-    // pass private key again. 
-    //     const encryptedJsonKey = await wallet.encrypt(
-    //         process.env.PRIVATE_KEY_PASSWORD,
-    //  )
+    ) */
+    // In later version (^6.2.3 as of this commit) of etherjs, PRIVATE_KEY is inferred from wallet, so there is no need to
+    // pass private key again.
+    const encryptedJsonKey = await wallet.encrypt(process.env.PRIVATE_KEY)
     console.log(encryptedJsonKey)
     fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey)
 }
